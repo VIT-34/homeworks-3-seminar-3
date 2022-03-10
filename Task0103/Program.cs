fa4b5b65da6f67f220ed3b18e40ac52bea714d6d
Vitaliy <viurum986@mail.ru>
2022-03-10 12:30:46 +0300

    Task0103

---
+++ Task0103/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine((number == 0) || (reverse(number, 0) == number));
+
+int reverse(int number, int i)
+
+{
+    while (number > 0)
+    {
+        i = i * 10 + number % 10; number /= 10;
+        
+    }
+
+    return i;
+}
