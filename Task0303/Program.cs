54db0649ee1eb95f1d5f90171076c04baf4feb06
Vitaliy <viurum986@mail.ru>
2022-03-10 14:01:07 +0300

    Task0303

---
+++ Task0303/Program.cs
@@ -0,0 +1,16 @@
+﻿int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int a = Promt("Введите число; ");
+int b = 1;
+Console.Write(b + " ");
+while (b < a)
+{
+    b = b + 1;
+    Console.Write(b*b*b + " ");
+}
